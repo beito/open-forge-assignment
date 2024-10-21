9f0909d13771f3875dfc22d55d520b5f5962d208
Julio <beito_xp@me.com>
2024-10-21 16:12:05 -0600

    Adding Highlight repos directive

---
+++ src/app/directives/highlight-repos.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightReposDirective } from './highlight-repos.directive';
+
+describe('HighlightReposDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightReposDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/highlight-repos.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlightRepos]',
+  standalone: true
+})
+export class HighlightReposDirective {
+
+  constructor() { }
+
+}
