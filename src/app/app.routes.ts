d6818aea3bb0012fd898e98a7924c41089de4d07
Julio <beito_xp@me.com>
2024-10-21 15:09:06 -0600

    Displaying Feed list

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { FeedComponent } from './pages/feed/feed.component';
+import { UserDetailComponent } from './pages/user-detail/user-detail.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
+  { path: '', redirectTo: 'feed', pathMatch: 'full' },
+  { path: 'feed', component: FeedComponent },
+  { path: 'user-detail', component: UserDetailComponent },
 ];
--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { GitHubService } from '../../services/github.service';
+import { GitHubUser } from '../../models/github-user.model';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
   standalone: true,
+  imports: [CommonModule, IonicModule, HttpClientModule],
 })
-export class FeedComponent  implements OnInit {
+export class FeedComponent implements OnInit {
+  users: GitHubUser[] = [];
 
-  constructor() { }
+  constructor(
+    private githubService: GitHubService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.githubService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
 
 }
