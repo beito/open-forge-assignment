b5091de70b717427a7904eca6d6bf8464dbd869b
Julio <beito_xp@me.com>
2024-10-21 14:24:41 -0600

    GitHub Users Interface created

---
+++ src/app/models/github-user.model.ts
@@ -0,0 +1,12 @@
+export interface GitHubUser {
+    id: number;
+    login: string;
+    avatar_url: string;
+    public_repos?: number;
+    name?: string;
+    bio?: string;
+    company?: string;
+    location?: string;
+    blog?: string;
+}
+  
