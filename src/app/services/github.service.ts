56d167f16383e654d5e9bff13b5b0b2ecd58f495
Julio <beito_xp@me.com>
2024-10-21 14:28:23 -0600

    Adding API consumption inside the service

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GitHubUser } from '../models/github-user.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class GithubService {
+export class GitHubService {
+  private readonly apiUrl = 'https://api.github.com/users'; // URL base de la API
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // Obtener lista de usuarios con paginación
+  getUsers(since: number = 0): Observable<GitHubUser[]> {
+    return this.http.get<GitHubUser[]>(`${this.apiUrl}?since=${since}`);
+  }
+
+  // Obtener detalles de un usuario específico por su nombre de usuario
+  getUserDetails(username: string): Observable<GitHubUser> {
+    return this.http.get<GitHubUser>(`${this.apiUrl}/${username}`);
+  }
 }
